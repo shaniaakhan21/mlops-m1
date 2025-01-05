fd04c2c6ba7517d7f938315ef89804c144c0ad66
Shaniya Khan <shaniaakhan21@gmail.com>
2025-01-05 22:01:48 +0400

    port fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -29,4 +30,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
